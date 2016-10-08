8933d7d554704935ddf63e274480a897714e6d81
Fabien Motte <contact@fabienmotte.com>
2016-10-09 00:03:31 +0200

    Global - Add budo server file

---
+++ server.js
@@ -0,0 +1,15 @@
+const budo = require('budo');
+
+budo('src/main.js', {
+  serve: 'build/bundle.js',
+  live: true,
+  open: true,
+  dir: 'public',
+  stream: process.stdout,
+  watchGlob: '**/*.{html,css,frag,vert,glsl}',
+  browserify: {
+    paths: [
+      __dirname + '/src'
+    ]
+  }
+});
